af4036d6aba8372a535c777fb719a72d27509033
matthewdes <tunedfrog@gmail.com>
2025-12-23 10:41:02 -0500

    dashboardinitialization

--- main_page/index.js
+++ main_page/index.js
@@ -1,4 +1,18 @@
 function themeToggle() {
     var element = document.body;
     element.classList.toggle("light-mode");
+
+    var x = document.getElementById("snackbar_theme-toggle");
+    x.className = "show";
+    setTimeout(function() {
+        x.className = x.className.replace("show", "");
+    }, 3000)
+
+    const lightToggle = document.getElementById("theme_toggle_button");
+    if (lightToggle.innerText === "Disabled") {
+        lightToggle.innerText = "Enabled";
+    } else {
+        lightToggle.innerText = "Disabled";
+    }
 }
+
